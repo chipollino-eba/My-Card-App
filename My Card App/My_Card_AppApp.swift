314959dd93800976d2f4f092cab87babd7f2f6f1
vadim.shamshin <vadim.shamshin1@yandex.ru>
2022-08-21 18:01:30 +0300

    Initial Commit

---
+++ My Card App/My_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Card_AppApp.swift
+//  My Card App
+//
+//  Created by Вадим Шамшин on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct My_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
